870e4ac6e1c82c206e37107f9295ec23aa842b9d
mreze <ericzelias@gmail.com>
2022-07-09 07:13:12 -0400

    Added Fader UI
    Adding Fading UI

---
+++ Assets/Scripts/UI/Fader.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fader : MonoBehaviour
+{
+
+    [SerializeField] CanvasGroup fadeGroup;
+    [SerializeField] float fadeTime;
+
+
+    public void SetAlpha(float alpha)
+    {
+        fadeGroup.alpha = alpha;
+    }
+
+    public void StartFadeIn()
+    {
+        StartCoroutine(FadeInCO());
+    }
+
+    public void StartFadeOut()
+    {
+        StartCoroutine(FadeOutCO());
+    }
+
+    public void StartFadeFromBlack()
+    {
+        StartCoroutine(FadeInFromBlackCO());
+    }
+
+
+    public IEnumerator FadeCO(float targetAlpha, float fadeTime)
+    {
+        float startAlpha = fadeGroup.alpha;
+
+        while (!Mathf.Approximately(fadeGroup.alpha, targetAlpha))
+        {
+            fadeGroup.alpha -= (startAlpha - targetAlpha) / fadeTime * Time.deltaTime;
+
+            yield return new WaitForSecondsRealtime(0);
+        }
+
+        fadeGroup.alpha = targetAlpha;
+    }
+
+    public IEnumerator FadeInCO() 
+    {
+        yield return FadeCO(0, fadeTime);
+    
+    
+    }
+
+    public IEnumerator FadeOutCO()
+    {
+        yield return FadeCO(1, fadeTime);
+    }
+
+    public IEnumerator FadeInFromBlackCO()
+    {
+        SetAlpha(1);
+        yield return FadeInCO();
+    }
+
+}
